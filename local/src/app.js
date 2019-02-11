6c2491884fc9dcd1b79f911e529e7ac143274454
Joe Meyer <jmeyer@smm.org>
2019-02-11 15:40:07 -0600

    moved over SteleLite App Template

---
+++ local/src/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var remote = require('electron').remote;
+
+var process = remote.process;
+
+//remote.getCurrentWindow().closeDevTools();
+
+var obtains = [];
+
+obtain(obtains, ()=> {
+
+  exports.app = {};
+
+  exports.app.start = ()=> {
+
+    console.log('started');
+
+    document.onkeypress = (e)=> {
+
+    };
+
+    document.onkeyup = (e)=> {
+      if (e.which == 73 && e.getModifierState('Control') &&  e.getModifierState('Shift')) {
+        remote.getCurrentWindow().toggleDevTools();
+      }
+    };
+
+    process.on('SIGINT', ()=> {
+      process.nextTick(function () { process.exit(0); });
+    });
+  };
+
+  provide(exports);
+});
