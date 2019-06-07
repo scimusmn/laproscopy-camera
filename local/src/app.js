dab2a965d488e15e49dd984615d865b90ddb1ec1
Joe Meyer <jmeyer@smm.org>
2019-06-07 12:08:19 -0500

    basic button function

--- local/src/app.js
+++ local/src/app.js
@@ -1,6 +1,9 @@
 'use strict';
-
+const Gpio = require('onoff').Gpio; // Import the onoff library
 var remote = require('electron').remote;
+
+var LED = new Gpio(27, 'out'); //use GPIO pin 4 as output
+var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
 var process = remote.process;
 
@@ -9,6 +12,22 @@ var process = remote.process;
 var obtains = [
   'µ/components/camera.js',
 ];
+
+pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+  if (err) { //if an error
+    console.error('There was an error', err); //output error message to console
+  return;
+  }
+  LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
+});
+
+function unexportOnClose() { //function to run when exiting program
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport LED GPIO to free resources
+  pushButton.unexport(); // Unexport Button GPIO to free resources
+};
+
+process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
 
 obtain(obtains, ()=> {
 
